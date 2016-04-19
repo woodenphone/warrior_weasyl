2026aff52821089b2bedc21b39d7b54408fbb32d
James <j1459.j1459@gmail.com>
2016-04-19 20:32:27 +0800

    added example for logins.py

---
+++ logins.py.example.py
@@ -0,0 +1,45 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      User
+#
+# Created:     19/04/2016
+# Copyright:   (c) User 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+
+
+WEASYL_LOGIN_DETAILS = [
+##    {'username':'',
+##    'password':'',
+##    'email':'',
+##    'email_password':'',
+##    'date_of_birth':'',
+##    'api_key':'',
+##    'last_used':None,
+##    'allocated':False,
+##    },
+    {'username':'',
+    'password':'',
+    'email':'',
+    'email_password':'',
+    'date_of_birth':'',
+    'api_key':'',
+    'last_used':None,
+    'allocated':False,
+    },
+    ]
+
+
+
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
